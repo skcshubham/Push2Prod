56d61cb714c8a5822d1053a26b9354ba6847c17b
skcshubham <mailtoshbhm@gmail.com>
2025-10-31 14:19:45 +0530

    base url change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 import type { User } from "../types/user.types";
 
-const API_BASE_URL = `http://${window.location.hostname}:8000`;
+const API_BASE_URL = `/api`;
 
 export const api = createApi({
   reducerPath: "api",
