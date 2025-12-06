412a350ba9ca97a9ac724caf80bfc9734da3639b
skcshubham <mailtoshbhm@gmail.com>
2025-12-06 14:54:05 +0530

    env fixes

--- frontend/src/config/socket.ts
+++ frontend/src/config/socket.ts
@@ -2,7 +2,11 @@ import { io } from "socket.io-client";
 import { API_BASE_URL } from "../services/api";
 
 export const createSocketConnection = () => {
-  return io(API_BASE_URL, {
-    autoConnect: true,
-  });
+  if (location.hostname === "localhost") {
+    return io(API_BASE_URL, {
+      autoConnect: true,
+    });
+  } else {
+    return io("/", { path: "/api/socket.io" });
+  }
 };
